14c30e2735e0e36b589a0105e6c27ba681203f73
ur5_usr <na>
2025-07-22 14:07:22 +0530

    Added hex_ft_sensor and world

--- launch/view_ur.launch.py
+++ launch/view_ur.launch.py
@@ -91,7 +91,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="ur.urdf.xacro",
+            default_value="ur_on_rail.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
